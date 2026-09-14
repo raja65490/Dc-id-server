1853ad6291449a5e3cdeb94257f871ab336fd9a0
raja65490 <rajmishra18908@outlook.com>
2026-09-14 07:41:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+global.WebSocket = require('ws');
 const express = require('express');
 const { MessengerClient, Platform, CookieManager } = require('messagix-js');
 const crypto = require('crypto');
